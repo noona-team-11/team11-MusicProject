8312afd1b152cb45e67e220f15041d498d375b34
jay0425 <heedev94@gmail.com>
2024-03-02 20:13:22 +0900

    추천 받은 앨범 커버 10곡 사진 뿌리기

--- musicInfo.js
+++ musicInfo.js
@@ -9,14 +9,48 @@ let keyword = '';
 let spotify_search_one = '';
 
 const btn = document.querySelector('.title-small');
-btn.addEventListener('click', function () {
+btn.addEventListener('click', async function () {
   let i = 0;
   for (i = 0; i < songList.length; i++) {
     keyword = songList[i].title + ' ' + songList[i].singer;
     console.log('재희의 테스트 : ', songList);
     console.log('재희의 테스트 : ', keyword);
     console.log(keyword);
-    startSpotify();
+
+    // Step 2: 키워드로 검색하기
+    spotify_search_one = await axios.get('https://api.spotify.com/v1/search', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+
+      params: {
+        q: keyword,
+
+        type: 'track',
+
+        limit: 1,
+      },
+    });
+
+    const render = () => {
+      songName = spotify_search_one.data.tracks.items[0].name;
+      artist = spotify_search_one.data.tracks.items[0].artists[0].name;
+      songImage = spotify_search_one.data.tracks.items[0].album.images[0].url;
+      albumList = spotify_search_one.data.tracks.items[0];
+      console.log(spotify_search_one);
+
+      const musicHTML = `
+        <div class="swiper-slide slide-list" data-swiper-autoplay="5000" data-video-idx="1" data-video-type="Youtube">
+                  <a href="#none" class="slide-item">
+                    <img class="slide-img" alt="앨범이미지" src=${songImage}>
+                  </a>
+                </div>
+          `;
+
+      document.querySelector('.swiper-wrapper').innerHTML += musicHTML;
+    };
+
+    // startSpotify();
     render();
   }
 });
@@ -37,41 +71,9 @@ const tokenResponse = await axios.post(
 
 const accessToken = tokenResponse.data.access_token;
 
-const startSpotify = async () => {
-  // Step 2: 키워드로 검색하기
-  spotify_search_one = await axios.get('https://api.spotify.com/v1/search', {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
+const startSpotify = async () => {};
 
-    params: {
-      q: keyword,
-
-      type: 'track',
-
-      limit: 10,
-    },
-  });
-};
-
-// console.log('필요한 데이터 : ', spotify_search_one);
-// console.log('필요한 데이터 : ', spotify_search_one.data.tracks.items[0].name);
-// console.log('필요한 데이터 : ', spotify_search_one.data.tracks.items[0].artists[0].name);
-// console.log('필요한 데이터 : ', spotify_search_one.data.tracks.items[0].album.images[0].url);
-
-const render = () => {
-  // songName = spotify_search_one.data.tracks.items[0].name;
-  // artist = spotify_search_one.data.tracks.items[0].artists[0].name;
-  // songImage = spotify_search_one.data.tracks.items[0].album.images[0].url;
-  // albumList = spotify_search_one.data.tracks.items[0];
-  // console.log(spotify_search_one);
-  const musicHTML = `
-    <div class="swiper-slide slide-list" data-swiper-autoplay="5000" data-video-idx="1" data-video-type="Youtube">
-              <a href="#none" class="slide-item">
-                <img class="slide-img" alt="앨범이미지" src=${spotify_search_one.data.tracks.items[0].album.images[1].url}>
-              </a>
-            </div>
-      `;
-
-  document.querySelector('.swiper-wrapper').innerHTML = musicHTML;
-};
+console.log('필요한 데이터 : ', spotify_search_one);
+console.log('필요한 데이터 : ', spotify_search_one.data.tracks.items[0].name);
+console.log('필요한 데이터 : ', spotify_search_one.data.tracks.items[0].artists[0].name);
+console.log('필요한 데이터 : ', spotify_search_one.data.tracks.items[0].album.images[0].url);
